2ea4121e2f958612e111dba2780f949c25cdcd82
a.korepanov <a.korepanov@skbkontur.ru>
2021-10-31 01:24:08 +0300

    init_commit

---
+++ enviroment.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import numpy as np
+from gym import spaces
+from typing import Tuple, Dict, Any
+
+sys.path.append(os.path.abspath("mapgen"))
+os.environ["PYTHONPATH"] = os.path.abspath("mapgen")
+
+from mapgen import Dungeon
+
+
+class ModifiedDungeon(Dungeon):
+    def __init__(self, width: int = 20, height: int = 20, max_rooms: int = 3,
+                 min_room_xy: int = 5, max_room_xy: int = 12,
+                 observation_size: int = 11, vision_radius: int = 5, max_steps: int = 2000):
+        super().__init__(width=width, height=height, max_rooms=max_rooms,
+                         min_room_xy=min_room_xy, max_room_xy=max_room_xy,
+                         observation_size=observation_size,
+                         vision_radius=vision_radius, max_steps=max_steps)
+        self.observation_space = spaces.Box(0, 1, [observation_size, observation_size, 3])
+        self.action_space = spaces.Discrete(3)
+
+    def step(self):
+        observation, reward, done, info = super().step()
+        observation = observation[:, :, :-1]  # remove trajectory
+        return observation, reward, done, info
---
+++ train.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import ray
+from hyperpyyaml import load_hyperpyyaml
+from ray import tune
+from PIL import Image
+
+
+def save_log(agent, config, iter_number):
+    env = config['env_class'](config['enf_config'])
+    obs = env.reset()
+    Image.fromarray(env._map.render(env._agent)).convert('RGB').resize((500, 500), Image.NEAREST).save('tmp.png')
+
+    frames = []
+
+    for _ in range(config.eval_steps):
+        action = agent.compute_single_action(obs)
+
+        frame = Image.fromarray(env._map.render(env._agent)).convert('RGB').resize((500, 500), Image.NEAREST).quantize()
+        frames.append(frame)
+
+        obs, reward, done, info = env.step(action)
+        if done:
+            break
+
+    gif_path = os.path.join(config['gifs_save_dir'], f"out{iter_number + 1}.gif")
+    frames[0].save(gif_path, save_all=True, append_images=frames[1:], loop=0, duration=1000 / 60)
+
+
+def train(config):
+    # Directory check
+    for dir in [config['checkpoint_save_dir'], config['gifs_save_dir']]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+    # Tuning enviroment
+    ray.shutdown()
+    ray.init(ignore_reinit_error=True)
+    tune.register_env(config['env_name'], lambda cfg: config['env_class'](**cfg))
+
+    agent = config['agent'](config['agent_config'])
+
+    for iter_number in range(config['n_iters']):
+        result = agent.train()
+        file_name = agent.save(config['checkpoint_save_dir'])
+
+        # logging
+        if (iter_number + 1) % 5 == 0:
+            save_log(agent, config, iter_number)
+
+
+if __name__ == "__main__":
+    # Reading config
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True,
+                        help='path to hyperpyyaml config')
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        cfg = load_hyperpyyaml(f)
+
+    train(cfg)
