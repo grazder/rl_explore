543701f5c693356766a9e3acbcd2228b1c2d065b
a.korepanov <a.korepanov@skbkontur.ru>
2021-10-31 12:23:48 +0300

    fix

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
+import os
 import argparse
 import ray
 import wandb
 from hyperpyyaml import load_hyperpyyaml
 from ray import tune
-from logging import log_metrics, save_gifs
+from log_utils import log_metrics, save_gifs
 
 
 def train(config):
@@ -22,11 +23,12 @@ def train(config):
     for iter_number in range(config['n_iters']):
         result = agent.train()
         _ = agent.save(config['checkpoint_save_dir'])
-        log_metrics(iter_number, result)
 
         # logging
+        log_metrics(iter_number, result)
+
         if (iter_number + 1) % 5 == 0:
-            save_log(agent, config, iter_number)
+            save_gifs(agent, config, iter_number)
 
 
 if __name__ == "__main__":
